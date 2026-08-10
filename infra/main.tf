e9763152f6af98700a633aef63cc79f2d34b531d
Dhiulia Antunes <antunesdhiulia@gmail.com>
2026-08-10 13:57:36 -0300

    Initial commit

--- infra/main.tf
+++
@@ -1,8 +0,0 @@
-resource "kubectl_manifest" "k8s" {
-  for_each  = fileset("${path.module}/../k8s", "*.yaml")
-  yaml_body = file("${path.module}/../k8s/${each.value}")
-
-  depends_on = [
-    kubernetes_namespace.oficina
-  ]
-}
--- infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "namespace" {
-  description = "Kubernetes namespace"
-  value       = kubernetes_namespace.oficina.metadata[0].name
-}
-
-output "environment" {
-  description = "Deployed environment"
-  value       = var.environment
-}
